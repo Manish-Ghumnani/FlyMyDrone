4adfa0aed51c52e07cd2e0b3357b26c960618441
Manish-Ghumnani <maniac3493@gmail.com>
2017-09-13 12:29:53 +0800

    Initial CodeBase

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-speechrecognition.SpeechRecognition",
+        "file": "plugins/cordova-plugin-speechrecognition/www/speechRecognition.js",
+        "pluginId": "cordova-plugin-speechrecognition",
+        "merges": [
+            "window.plugins.speechRecognition"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-speechrecognition": "1.1.2"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-speechrecognition/www/speechRecognition.js",
+        "id": "cordova-plugin-speechrecognition.SpeechRecognition",
+        "pluginId": "cordova-plugin-speechrecognition",
+        "merges": [
+            "window.plugins.speechRecognition"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-speechrecognition": "1.1.2"
+}
+// BOTTOM OF METADATA
+});
